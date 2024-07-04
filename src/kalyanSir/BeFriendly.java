56294bef99767900b0b55b67644456ade193d20f
pittamallasreenu <158571892+pittamallasreenu@users.noreply.github.com>
2024-07-04 20:45:17 +0530

    No one can takecareof you

---
+++ src/kalyanSir/BeFriendly.java
@@ -0,0 +1,13 @@
+package kalyanSir;
+
+public class BeFriendly {
+
+	public static void main(String[] args)
+	{
+		System.out.println("our main agenda is to teach core "
+				+ "fuctionality like as primary colours");
+		System.out.println("Arif is a good person");
+
+	}
+
+}
