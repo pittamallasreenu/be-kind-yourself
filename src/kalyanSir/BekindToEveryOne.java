3a7bbd8ae130bba1b118fff6aa39d4ce20fc9756
pittamallasreenu <158571892+pittamallasreenu@users.noreply.github.com>
2024-07-04 21:07:42 +0530

    everyone's mentality not same

--- src/kalyanSir/BekindToEveryOne.java
+++ src/kalyanSir/BekindToEveryOne.java
@@ -2,8 +2,9 @@ package kalyanSir;
 
 public class BekindToEveryOne {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		System.out.println("the fruits of patience is sweet");
 
 	}
 
